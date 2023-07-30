f0c4f02b8d48e3ce94b326357f0fbf4e14f27b49
Maxime Tan <maxime.tan1@gmail.com>
2023-07-30 20:55:23 +0200

    update

--- app/public/wp-content/themes/blankslate-child/header.php
+++ app/public/wp-content/themes/blankslate-child/header.php
@@ -4,8 +4,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;800&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
